df7d7dcfcd9f5d8842d674958cfcb4fbaabe6dfd
josh <josh@tesseractmobile.com>
2020-08-06 23:50:50 -0500

    Fixed url

--- react-app/src/UnsplashImage.js
+++ react-app/src/UnsplashImage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class UnsplashImage extends Component {
   render() {
     var { regular, small } = this.props.image.urls;
-    small = 'http://placekitten.com/200/200';
+    //small = 'http://placekitten.com/200/200';
     const key = this.props.image.id;
     const alt = this.props.image.alt;
     return (
